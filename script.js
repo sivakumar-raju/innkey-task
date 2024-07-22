8ef24b9e51b8182c41e6e3e398fbbb6e2c7e3581
NandimandalamS <sivakumar.nandimandalam@agilysys.com>
2024-07-22 21:07:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Interview Task Page Loaded');
+});
+
+
+function customerLogin() {
+    alert("Login Implementation");
+}
+
+function demo() {
+    alert("demo Implementation");
+}
+
+function enquiry() {
+    alert("enquiry Implementation");
+}
+
+
